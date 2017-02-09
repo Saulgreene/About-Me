1724e597a9842796e0d35f66be1ac291310744b9
Saul Greene <spdysg@gmail.com>
2017-02-08 17:18:42 -0800

    forgot to un-comment the js file on last push

--- app.js
+++ app.js
@@ -1,46 +1,46 @@
-// 'use strict';
-//
-// var confirmQuiz = confirm('Hello, would you like to take a quiz?');
-// if (confirmQuiz){
-//   console.log('Awesome, lets get started!');
-// }else{
-//   console.log('Too Bad, starting anyway.');
-// }
-//
-// var userName = prompt('Before we get started, what is your name?');
-// console.log(userName);
-//
-// var quizResponseOne = prompt('Thanks' + ' ' + userName + ' ' + 'Please answer with Yes or No. Does Saul have a dog?');
-// if (quizResponseOne.toUpperCase() === 'YES'){
-//   console.log('Awesome, you are correct!');
-// }else{
-//   console.log('Too Bad, you were wrong. Saul has a dog');
-// }
-//
-// var quizResponseTwo = prompt('Please answer with Yes or No. Does Saul play any sports?');
-// if (quizResponseTwo.toUpperCase() === 'YES'){
-//   console.log('Awesome, you are correct! Snowboarding ALL DAY!!');
-// }else{
-//   console.log('Too Bad, you were wrong. It\`s ok, most poeple wouldnt think he snowboards.');
-// }
-//
-// var quizResponseThree = prompt('Please answer with Yes or No. Does Saul own a car?');
-// if (quizResponseThree.toUpperCase() === 'YES'){
-//   console.log('Awesome, you are correct! He loves that thing!');
-// }else{
-//   console.log('Too Bad, you were wrong. How could you get that wrong? All he talks about is that car.');
-// }
-//
-// var quizResponseFour = prompt('Please answer with Yes or No. Is Saul new to coding?');
-// if (quizResponseFour.toUpperCase() === 'YES'){
-//   console.log('Correct! But not for long.');
-// }else{
-//   console.log('Too Bad, you were wrong.');
-// }
-//
-// var quizResponseFive = prompt('Please answer with Yes or No. Does Saul like Code Fellows?');
-// if (quizResponseFive.toUpperCase() === 'YES'){
-//   console.log('Correct! OF COURSE HE DOES!!!');
-// }else{
-//   console.log('Wrong! He loves Code Fellows');
-// }
+'use strict';
+
+var confirmQuiz = confirm('Hello, would you like to take a quiz?');
+if (confirmQuiz){
+  console.log('Awesome, lets get started!');
+}else{
+  console.log('Too Bad, starting anyway.');
+}
+
+var userName = prompt('Before we get started, what is your name?');
+console.log(userName);
+
+var quizResponseOne = prompt('Thanks' + ' ' + userName + ' ' + 'Please answer with Yes or No. Does Saul have a dog?');
+if (quizResponseOne.toUpperCase() === 'YES'){
+  console.log('Awesome, you are correct!');
+}else{
+  console.log('Too Bad, you were wrong. Saul has a dog');
+}
+
+var quizResponseTwo = prompt('Please answer with Yes or No. Does Saul play any sports?');
+if (quizResponseTwo.toUpperCase() === 'YES'){
+  console.log('Awesome, you are correct! Snowboarding ALL DAY!!');
+}else{
+  console.log('Too Bad, you were wrong. It\`s ok, most poeple wouldnt think he snowboards.');
+}
+
+var quizResponseThree = prompt('Please answer with Yes or No. Does Saul own a car?');
+if (quizResponseThree.toUpperCase() === 'YES'){
+  console.log('Awesome, you are correct! He loves that thing!');
+}else{
+  console.log('Too Bad, you were wrong. How could you get that wrong? All he talks about is that car.');
+}
+
+var quizResponseFour = prompt('Please answer with Yes or No. Is Saul new to coding?');
+if (quizResponseFour.toUpperCase() === 'YES'){
+  console.log('Correct! But not for long.');
+}else{
+  console.log('Too Bad, you were wrong.');
+}
+
+var quizResponseFive = prompt('Please answer with Yes or No. Does Saul like Code Fellows?');
+if (quizResponseFive.toUpperCase() === 'YES'){
+  console.log('Correct! OF COURSE HE DOES!!!');
+}else{
+  console.log('Wrong! He loves Code Fellows');
+}
